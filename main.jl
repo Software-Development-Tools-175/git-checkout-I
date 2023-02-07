3ca698d1f59ce36a7867dbf86a31599218c4751c
santhitak <63070175@kmitl.ac.th>
2023-02-07 14:37:40 +0700

    feat: fourth commit with pattern readline

--- main.jl
+++ main.jl
@@ -2,6 +2,8 @@ println("Welcome to week 4: git checkout\n")
 println("This is commit one\n")
 println("This line is added for commit two\n")
 println("This line is added for commit three\n")
+print("Here is commit four, please enter your desired pattern : ")
+pattern = readline()
 
 function mandelbrot(a)
     z = 0
@@ -13,7 +15,7 @@ end
 
 for y=1.0:-0.05:-1.0
     for x=-2.0:0.0315:0.5
-        abs(mandelbrot(complex(x, y))) < 2 ? print("*") : print(" ")
+        abs(mandelbrot(complex(x, y))) < 2 ? print(pattern) : print(" ")
     end
     println()
 end
